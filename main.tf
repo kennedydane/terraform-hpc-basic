127947b5ca25996f736829344d6c29c8bc8376e0
Dane Kennedy <kennedy.dane@gmail.com>
2019-06-13 15:47:22 +0200

    Do upgrade, not update
    Also add in DEBIAN_FRONTEND=noninteractive environmental variable so that apt-get explicitly knows not to prompt for input.

--- main.tf
+++ main.tf
@@ -80,8 +80,8 @@ resource "openstack_compute_floatingip_associate_v2" "headnode_floating_ip" {
     inline = [
       "echo 'Beginning the provisioner exec....'",
       "sudo apt-get -y update",
-      "sudo apt-get -y update",
-      "sudo apt-get -y install python3-pip wget unzip",
+      "sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade",
+      "sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python3-pip wget unzip",
       "sudo pip3 -q install ansible",
     ]
     connection {
